05d6c72df57372d8dc43495358d142b75d1612c2
kh4l1d <khalidahmedsayed@gmail.com>
2020-01-11 11:48:31 +0530

    Create day-11-bit-addition.java

---
+++ day-11-bit-addition.java
@@ -0,0 +1,18 @@
+// Problem statement = https://leetcode.com/problems/sum-of-two-integers
+
+class Solution 
+{
+    public int getSum(int a, int b) 
+    {
+        while (b != 0)
+        {
+            int carry = a & b;
+            
+            a = a ^ b;
+            
+            b = carry << 1;
+        }
+        
+        return a;
+    }
+}
