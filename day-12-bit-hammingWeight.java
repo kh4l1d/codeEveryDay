5159db6e1efeca3b74d4cdbc0d9e27b4cac698d1
kh4l1d <khalidahmedsayed@gmail.com>
2020-01-12 22:02:52 +0530

    Create day-12-bit-hammingWeight.java

---
+++ day-12-bit-hammingWeight.java
@@ -0,0 +1,20 @@
+// Problem statement = https://leetcode.com/problems/number-of-1-bits
+
+public class Solution 
+{
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) 
+    {
+        int bits = 0, mask = 1;
+        
+        for(int i=0;i<32;i++)
+        {
+            if((n & mask) != 0)
+                bits++;
+            
+            mask <<= 1;
+        }
+        
+        return bits;
+    }
+}
